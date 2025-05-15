b4a887be6a2c2f038f63c0b1f31d7f7f58266b59
Sergey Sukharev <suharev.s@icloud.com>
2025-05-15 18:48:28 +0300

    Created projects files

---
+++ choiceOfActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choiceOfActions
+//
+//  Created by Sergey Sukharev on 15.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
